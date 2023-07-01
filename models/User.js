b51fc397912de931e34843abfbb243ac981fd5e5
alefra88 <efra.rlz@gmail.com>
2023-06-30 22:01:17 -0600

    mongoose pre save

--- models/User.js
+++ models/User.js
@@ -1,29 +1,33 @@
 const mongoose = require("mongoose"),
+  bcrypt = require("bcryptjs");
 
+UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "please provide a valid password"],
+    minlength: 6,
+  },
+});
+//Hash password
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  
+});
 
-  UserSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, "please provide name"],
-      minlength: 3,
-      maxlength: 50,
-    },
-    email: {
-      type: String,
-      required: [true, "please provide email"],
-      match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "please provide a valid email",
-      ],
-      unique:true,
-    },
-    password: {
-      type: String,
-      required: [true, "please provide a valid password"],
-      minlength: 6,
-      
-    },
-  });
-
-
-  module.exports = mongoose.model('User,',UserSchema)
+module.exports = mongoose.model("User,", UserSchema);
