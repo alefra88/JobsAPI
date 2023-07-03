933de1e9ce10669b252ffdc33316f46d6583ce64
alefra88 <efra.rlz@gmail.com>
2023-07-03 04:18:58 -0600

    login logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { badRequestError } = require("../errors");
+const { badRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
@@ -8,7 +8,17 @@ const register = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
   },
   login = async (req, res) => {
-    res.send("login user");
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new badRequestError(`please provide email and password`);
+    }
+    const user = await User.findOne({ email });
+    //compare password
+    if (!user) {
+      throw new UnauthenticatedError("Invalid credentials");
+    }
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
   };
 
 module.exports = {
