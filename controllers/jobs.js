f9be08849750d7c4acee36dba5fb4bb95194f0b0
alefra88 <efra.rlz@gmail.com>
2023-07-06 23:04:21 -0600

    little changes

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,21 @@
+const Job = require("../models/Job"),
+  { StatusCodes } = require("http-status-codes"),
+  { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-    res.send("get all jobs...");
+    const jobs = await job
+      .find({ createdBy: req.res.userId })
+      .sort("createdAt");
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
   },
   getJob = async (req, res) => {
     res.send("getting job...");
   },
   createJob = async (req, res) => {
-    res.json(req.user);
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body);
+    res.status(StatusCodes.CREATED).json({ job });
+    res.json(req.body);
   },
   updateJob = async (req, res) => {
     res.send("updating job...");
