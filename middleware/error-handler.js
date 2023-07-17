16f28010019badb5be73c23582293ecd523ec9bd
alefra88 <efra.rlz@gmail.com>
2023-07-17 11:04:21 -0600

    error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,4 @@
-const { CustomAPIError } = require("../errors");
+// const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -7,8 +7,15 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "something get wrong try again later",
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === "ValidatorError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
   }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
@@ -16,8 +23,8 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field,please choose another value`;
     customError.statusCode = 400;
   }
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
-  return res.status(customError.statusCode).json({ msg: customError.msg });
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  // return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
