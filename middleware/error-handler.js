cb05d1637652d02db71b55d226a0ac1153542a4f
alefra88 <efra.rlz@gmail.com>
2023-07-21 01:05:51 -0600

    cast errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -12,7 +12,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // }
 
   if (err.name === "ValidationError") {
-    console.log(Object.values(err.errors))
+    console.log(Object.values(err.errors));
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
       .join(",");
@@ -23,6 +23,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       err.keyValue
     )} field,please choose another value`;
     customError.statusCode = 400;
+  }
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id ${err.value}`;
+    customError.statusCode = 404;
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
