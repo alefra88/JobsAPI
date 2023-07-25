38eaee2fb42777c196eea9cf2aaa3656771bb639
alefra88 <efra.rlz@gmail.com>
2023-07-24 23:24:30 -0600

    adding some security stuffs

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
 
+//extra security package
+const helmet = require("helmet"),
+  cors = require("cors"),
+  xss = require("xss-clean"),
+  rateLimiter = require("express-rate-limit");
+///
+
 const express = require("express"),
   app = express(),
   morgan = require("morgan");
@@ -16,15 +23,23 @@ const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    widowsMs: 15 * 60 * 1000, // 15min
+    max: 100, //limit each ip to 100 request per windowMs
+  })
+);
 app.use(express.json());
-
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 app.use(morgan("dev"));
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs",authenticateUser, jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
